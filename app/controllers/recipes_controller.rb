6d0af21f586d00437d7e313dfa3dff32067bf508
lalogaribo <lalo.garibo90@gmail.com>
2018-03-08 08:00:31 -0600

    Added edit and destroy methods

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -23,6 +23,28 @@ class RecipesController < ApplicationController
     end
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = 'Recipe was updated successfully'
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    if @recipe.destroy
+      flash[:success] = 'Recipe was deleted successfully'
+      redirect_to recipes_path
+    end
+  end
+
   private
 
   def recipe_params
---
+++ test/integration/recipes_edit_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class RecipesEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @chef = Chef.create(chefname: 'Jose Eduardo', email: 'jose@example.com')
+    @recipe = Recipe.create(name: 'Tacos al vapor',
+                            description: 'Buenos tacos de cabeza', chef: @chef)
+  end
+
+  test 'reject invalid recipe update' do
+      get edit_recipe_path(@recipe)
+      assert_template 'recipes/edit'
+      patch recipe_path(@recipe), params: { recipe: { name: '', description: 'some desc'}}
+      assert_template 'recipes/edit'
+      assert_select 'h2.panel-title'
+      assert_select 'div.panel-body'
+  end
+
+  test 'successfully edit a recipe' do
+      get edit_recipe_path(@recipe)
+      assert_template 'recipes/edit'
+      name = 'taquitos de asada'
+      description = 'carne de res'
+      patch recipe_path(@recipe), params: { recipe: { name: name, description: description}}
+      assert_redirected_to @recipe
+      assert_not flash.empty?
+      @recipe.reload
+      assert_match name, @recipe.name
+      assert_match description, @recipe.description1
+  end
+end
