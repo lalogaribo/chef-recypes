cb5df414f1c6ce9694bd4219490481e32f641f8d
lalogaribo <lalo.garibo90@gmail.com>
2018-03-10 16:23:02 -0600

    Added delete chef action

--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -71,4 +71,12 @@ class ChefTest < ActiveSupport::TestCase
     @chef.password = @chef.password_confirmation = "x" * 4
     assert_not @chef.valid?
   end
+
+  test 'associated recipes should be removed' do
+    @chef.save
+    @chef.recipes.create!(name: 'testing destroy', description: 'testing destroy')
+    assert_difference 'Recipe.count', -1 do
+      @chef.destroy
+    end
+  end
 end
